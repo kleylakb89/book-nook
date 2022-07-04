1773da4cec5f9bc85e978e77b2cbb7f0f9689683
cdt96 <cody.theroux3@gmail.com>
2022-07-04 12:14:07 -0400

    filled out my index.js in api folder and started a router sandwich in all of my api routes files

---
+++ controllers/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const bookRoutes = require('./book-routes');
+const favoriteRoutes = require('./favorite-routes');
+const genreRoutes = require('./genre-routes');
+const reviewRoutes = require('./review-routes');
+const shelfRoutes = require('./shelf-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/books', bookRoutes);
+router.use('/favorites', favoriteRoutes);
+router.use('/genres', genreRoutes);
+router.use('/reviews', reviewRoutes);
+router.use('/shelves', shelfRoutes);
+router.use('/users', userRoutes);
+
+
+module.exports = router;
