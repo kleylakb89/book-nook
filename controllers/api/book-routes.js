a199c435ff34e2dad7b4c7be65651d609a500f9c
cdt96 <cody.theroux3@gmail.com>
2022-07-05 17:38:37 -0400

    brought book.findone from library-routes to book-routes, commented out the get all book routes from book-routes since i have a get all books route on library-routes

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -42,16 +42,69 @@ router.delete('/:id', withAuth, (req, res) => {
 });
 
 //view all books
-router.get('/', withAuth, (req, res) => {
-  Book.findAll()
-    .then((dbBookData) => res.json(dbBookData))
+// router.get('/', withAuth, (req, res) => {
+//   Book.findAll()
+//     .then((dbBookData) => res.json(dbBookData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+//view a single book
+
+router.get("/:id", (req, res) => {
+  Book.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "title",
+      "author",
+      "has_read",
+      "favorite",
+      "user_id",
+      "genre_id",
+      "review_id",
+    ],
+    include: [
+      {
+        model: Review,
+        attributes: ["id", "rating", "text", "user_id", "book_id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBookData) => {
+      if (!dbBookData) {
+        res.status(404).json({
+          message: "No book found with this id",
+        });
+        return;
+      }
+
+      const book = dbBookData.get({
+        plain: true,
+      });
+
+      res.render("single-book", {
+        book,
+        loggedIn: req.session.loggedIn,
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-
-//view a single book
 
 //update a book
 
--- controllers/library-routes.js
+++ controllers/library-routes.js
@@ -47,58 +47,6 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/books/:id", (req, res) => {
-  Book.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      "id",
-      "title",
-      "author",
-      "has_read",
-      "favorite",
-      "user_id",
-      "genre_id",
-      "review_id",
-    ],
-    include: [
-      {
-        model: Review,
-        attributes: ["id", "rating", "text", "user_id", "book_id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbBookData) => {
-      if (!dbBookData) {
-        res.status(404).json({
-          message: "No book found with this id",
-        });
-        return;
-      }
-
-      const book = dbBookData.get({
-        plain: true,
-      });
-
-      res.render("single-book", {
-        book,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
 
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
